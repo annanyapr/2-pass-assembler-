860562b9ed4271fe42a66570ef23c73404478b06
Annanya <annanyapr@gmail.com>
2019-02-17 01:16:48 +0530

    First Pass Done

--- assembler.c
+++ assembler.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 
-FILE * file = fopen("");
+//FILE * file = fopen("");
 int address = 0;
 
 int bin[4];
@@ -13,59 +13,67 @@ int bin[4];
 // searches for a particular string in a file 
 int search_in_file(char * fname, char* strname){
 	FILE * fp;
-	fp = fopen(fname, "r");
+	fp = fopen(fname, "a+");
+	fseek(fp, 0, SEEK_SET);
 	char* line = NULL;
 	size_t len = 0;
-	ssize_t read;
+	
 	int length = 0;
-	int flag = 0;
-	while((read = getline(&line, &len, fp)) != -1){
-		length = strlen(line);
-		line[length-1] = '\0';
+	
+	while((getline(&line, &len, fp)) != -1){
+
 		if (strstr(line , strname) != NULL){
-			flag = 1;
+			
+			fclose(fp);
+			return(1);
 		}
+		
 	}
-
+	
 	fclose(fp);
+	return(0);
 
-	if (flag = 1){
-		return(1);
-	}
-	else{
-		return(0);
-	}
 }
 
 
 
 
 
-int opcode(char * str, int len){
+int opcode(char * str){
 
-	
+	FILE * f;
 	char opcd[5];
+	int i = 0;
 	while (str[i] == ' ' ){
 		
 		i++;
 
 	}
 	
-	if (str[i] != '\n'){
+	if (str[i] != '\0'){
 
 		// execute thenormal routine
 		
 		int	j = 0; 
-		while (str[i] != ' '){
+		while (str[i] != ' ' && str[i] != '\0'){
 			opcd[j] = str[i];
+			i++;
 			j++;
 		}
+		str = str + i;
 		opcd[j] = '\0';
+		if (strcmp(opcd, "LOOP") == 0){
+			
+			opcode(" SUB H");
+			opcode(" JNZ ");
+			return(0);
+		}
 
 		// now we have both the opcode and the length of the opcode stored in j
 		// we have a string in opcode 
 		// first we will search in file
 		// now we will use strcmp with all the possible opcodes to analyse the string
+
 
 		if (search_in_file("opTable.txt", opcd) == 1){
 			// string already exist
@@ -78,92 +86,219 @@ int opcode(char * str, int len){
 			for(int i = 0; i < j; i++){
 				fputc(opcd[i], f);
 			}
-			fputc(" ", f);
-			fputc(" ", f);
-			int temp = address;
-			char c;
-	
-			for(int i = 0; i < 4; i++){
-				bin[3-i] = temp%16;
-				temp = temp / 16;
-
-			}
-
-	
+			
+			fputc(' ', f);
+			fputc(' ', f);
+			
 
 
-			for (int i = 0; i < 4; i++){
-				switch(bin[i]){
-					case 10: c = 'A';
-						break;
 
-					case 11: c = 'B';
-						break;
 
-					case 12: c = 'C';
-						break;
-
-					case 13: c = 'D';
-						break;
-					
-					case 14: c = 'E';
-						break;
-					
-					case 15: c = 'F';
-						break;
-					
-					default: c = bin[i] + '0';
-						break;
-				}
-
-				fputc(c, f);
-			}
 
 			// now we need to increment the address by the proper amount // this will depend upon the opcode value + the type of inputs provided to us
 			if (strcmp(opcd, "MOV") == 0){
-				address += 4;
+				if (strstr(str, "H") != NULL){ // transfer the value directly to the register
+					address += 4;
+				}
+				else if (strstr(str, "B") != NULL){
+					address += 4;
+				}
+				else if (strstr(str, "D") != NULL){
+					address += 4;
+				}
+				else{ // register to register
+					address += 2;
+				}
+				fputc('0', f);
+				fputc('0', f);
+				fputc('0', f);
+				fputc('0', f);
+				fputc('\n', f);
 			}
+
 			else if (strcmp(opcd, "ADD") == 0){
-				address += 4;
+				if (strstr(str, "H") != NULL){ // transfer the value directly to the register
+					address += 4;
+				}
+				else if (strstr(str, "B") != NULL){
+					address += 4;
+				}
+				else if (strstr(str, "D") != NULL){
+					address += 4;
+				}
+				else{ // register to register
+					address += 2;
+				}
+
+				fputc('0', f);
+				fputc('0', f);
+				fputc('0', f);
+				fputc('1', f);
+				fputc('\n', f);
 
 
 			}
+
 			else if (strcmp(opcd, "SUB") == 0){
-				address += 4;
+				//ddress += 4;
+				if (strstr(str, "H") != NULL){ // transfer the value directly to the register
+					address += 4;
+				}
+				else if (strstr(str, "B") != NULL){
+					address += 4;
+				}
+				else if (strstr(str, "D") != NULL){
+					address += 4;
+				}
+				else{ // register to register
+					address += 2;
+				}
 
-
+				fputc('0', f);
+				fputc('0', f);
+				fputc('1', f);
+				fputc('0', f);
+				fputc('\n', f);
 			}
+
 			else if (strcmp(opcd, "OR") == 0){
-				address += 2;
+				if (strstr(str, "H") != NULL){ // transfer the value directly to the register
+					address += 4;
+				}
+				else if (strstr(str, "B") != NULL){
+					address += 4;
+				}
+				else if (strstr(str, "D") != NULL){
+					address += 4;
+				}
+				else{ // register to register
+					address += 2;
+				}
 
-
+				fputc('0', f);
+				fputc('1', f);
+				fputc('1', f);
+				fputc('0', f);
+				fputc('\n', f);
 			}
-			else if (strcmp(opcd, "AND") == 0){
-				address += 2;
 
+			else if (strcmp(opcd, "AND") == 0){
+				if (strstr(str, "H") != NULL){ // transfer the value directly to the register
+					address += 4;
+				}
+				else if (strstr(str, "B") != NULL){
+					address += 4;
+				}
+				else if (strstr(str, "D") != NULL){
+					address += 4;
+				}
+				else{ // register to register
+					address += 2;
+				}
+
+				fputc('0', f);
+				fputc('1', f);
+				fputc('0', f);
+				fputc('1', f);
+				fputc('\n', f);
 				
 			}
+
 			else if (strcmp(opcd, "CMP") == 0){
-				address += 4;
+				if (strstr(str, "H") != NULL){ // transfer the value directly to the register
+					address += 4;
+				}
+				else if (strstr(str, "B") != NULL){
+					address += 4;
+				}
+				else if (strstr(str, "D") != NULL){
+					address += 4;
+				}
+				else{ // register to register
+					address += 2;
+				}
+
+				fputc('0', f);
+				fputc('1', f);
+				fputc('0', f);
+				fputc('0', f);
+				fputc('\n', f);
 			}
+
+
 			else if (strcmp(opcd, "MUL") == 0){
+				if (strstr(str, "H") != NULL){ // transfer the value directly to the register
+					address += 4;
+				}
+				else if (strstr(str, "B") != NULL){
+					address += 4;
+				}
+				else if (strstr(str, "D") != NULL){
+					address += 4;
+				}
+				else{ // register to register
+					address += 2;
+				}
 
+				fputc('0', f);
+				fputc('0', f);
+				fputc('1', f);
+				fputc('1', f);
+				fputc('\n', f);
 			}
+
 			else if (strcmp(opcd, "JMP") == 0){
+				address += 3;
 
+				fputc('1', f);
+				fputc('0', f);
+				fputc('0', f);
+				fputc('0', f);
+				fputc('\n', f);
 			}
+
 			else if (strcmp(opcd, "JNZ") == 0){
+				address += 3;
 
+				fputc('1', f);
+				fputc('0', f);
+				fputc('1', f);
+				fputc('1', f);
+				fputc('\n', f);
 			}
-			else if (strcmp(opcd, "NOT") == 0){
 
+			else if (strcmp(opcd, "NOT") == 0){  // only take the register case 
+				address += 2;
+
+				fputc('0', f);
+				fputc('1', f);
+				fputc('1', f);
+				fputc('1', f);
+				fputc('\n', f);
 			}
+
 			else if (strcmp(opcd, "HLT") == 0){
+				address += 1;
 
+				fputc('1', f);
+				fputc('0', f);
+				fputc('1', f);
+				fputc('0', f);
+				fputc('\n', f);
 			}
+
 			else if (strcmp(opcd, "LOOP") == 0){
+				address += 7;
 
+				fputc('1', f);
+				fputc('0', f);
+				fputc('0', f);
+				fputc('1', f);
+				fputc('\n', f);
 			}
+
+			fclose(f);
+
 
 		}
 
@@ -175,7 +310,8 @@ int opcode(char * str, int len){
 
 	}
 
-	fclose(f);
+	
+	return(0);
 
 
 // will write for the opcode 
@@ -185,10 +321,10 @@ int opcode(char * str, int len){
 
 
 
-void label(char* str , int len){
+int label(char* str){
 
 // find the label and write in the text file 
-
+	FILE * f;
 	int i = 0;
 	while (str[i] == ' '){
 		i++;
@@ -200,7 +336,7 @@ void label(char* str , int len){
 	
 	f = fopen("symTable.txt", "a+");
 
-	while (str[i] != ' ' || str[i] != '\0'){
+	while (str[i] != ':' &&  str[i] != '\0'){
 		fputc(str[i], f);
 		i++;
 	}
@@ -247,12 +383,18 @@ void label(char* str , int len){
 	}
 
 	fputc('H', f);
+	fputc(' ', f);
 	fputc('\n', f);
 	fclose(f);
 	str = str + i;
+	if (str[0] == ':'){
+		str++;
+	}
 	// now str has been shifted to the point where str[0] points to the point which is the first character after the label 
 	// now written the code for adding the label with the required address field
-	opcode(str, len);
+	opcode(str);
+
+	return(0);
 
 
 }
@@ -263,7 +405,7 @@ void label(char* str , int len){
 
 
 
-int func(char * str, int len){
+int func(char * str){
 
 	// now check the first word in the string
 
@@ -272,15 +414,17 @@ int func(char * str, int len){
 		i++;
 	}
 
-	while(str[i] != ' ')}{
+	while(str[i] != ' '){
 		i++;
 	}
+
 	if (str[i-1] == ':'){
-		label(str, len);
+		label(str);
 	}
 	else{
-		opcode(str, len);
+		opcode(str);
 	}
+	return(0);
 
 
 }
@@ -299,7 +443,7 @@ int main(){
 	while ((read = getline(&linechars, &length, f)) != -1) {
         //len = strlen(linechars); // gives the length of the string array with the new line in place 
         //printf("%s %d", linechars, len);
-		len  =strlen(linechars);
+		len = strlen(linechars);
 
 		linechars[len-1] = '\0';
 
@@ -307,6 +451,7 @@ int main(){
 			if (flag == 0){ 
 
 				flag = 1;
+				linechars = NULL;
 				continue;
 
 			}
@@ -314,12 +459,12 @@ int main(){
 
 			// call the function giving them values 
 
-			func(linechars, len);
+			func(linechars);
 
 
 		}
 
-
+		linechars = NULL;
 
 
     }
